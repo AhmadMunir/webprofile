a78e2891de4e613e3cb2ff064c6af5cc2e59dc07
AhmadMunir <hel000941@gmail.com>
2020-09-12 19:36:15 +0700

    update

--- application/controllers/Sutol.php
+++ application/controllers/Sutol.php
@@ -1,6 +1,6 @@
 <?php
   defined('BASEPATH') OR exit('No direct script access allowed');
-  class Accomodation extends CI_Controller
+  class Sutol extends CI_Controller
   {
     public function index(){
       $this->load->view('accomodation');
--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <a class="navbar-brand logo_h" href="<?php echo base_url() ?>"><img src="<?php echo base_url('assets/') ?>image/Logo.png" alt=""></a>
+            <!-- <a class="navbar-brand logo_h" href="<?php echo base_url() ?>"><img src="<?php echo base_url('assets/') ?>image/Logo.png" alt=""></a> -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -12,18 +12,57 @@
             <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                 <ul class="nav navbar-nav menu_nav ml-auto">
                     <li class="nav-item active"><a class="nav-link" href="<?php echo base_url() ?>">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('About') ?>">About us</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="<?php echo base_url('About') ?>">About us</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo base_url('Accomodation') ?>">Accomodation</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('Gallery') ?>">Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('Gallery') ?>">Gallery</a></li> -->
                     <li class="nav-item submenu dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil Desa</a>
                         <ul class="dropdown-menu">
-                            <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Sejarah Desa</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Profil Wilayah Desa</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('Elemests') ?>">Elemests</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pemerintahan Desa</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Visi & Misi Desa</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Pemerintah Desa</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BUMDES</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Tentang BUMDES</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Unit BUMDES</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Lembaga Masyarakat</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Karang Taruna</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">PKK</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Data Desa</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Data Wilayah Administratif</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Data Pendidikan Wilayah</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Data Pekerjaan</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Data Jenis Kelamin</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Data Kelompok Umur</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Data Perkawinan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Informasi Geografis</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Lokasi Desa</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Persebaran Penduduk</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Tempat Ibadah</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('Sutol') ?>">Surat Online</a></li>
                 </ul>
             </div>
         </nav>
