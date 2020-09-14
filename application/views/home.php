0007558d7fa40d35501be604258bdab52fa2d006
adeas10 <44155091+adeas10@users.noreply.github.com>
2020-09-13 19:59:40 -0700

    v1

--- application/views/home.php
+++ application/views/home.php
@@ -21,7 +21,7 @@
 					</div>
 				</div>
             </div>
-            <div class="hotel_booking_area position">
+            <!-- <div class="hotel_booking_area position">
                 <div class="container">
                     <div class="hotel_booking_table">
                         <div class="col-md-3">
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </section>
         <!--================Banner Area =================-->
 
@@ -96,48 +96,68 @@
         <section class="accomodation_area section_gap">
             <div class="container">
                 <div class="section_title text-center">
-                    <h2 class="title_color">Hotel Accomodation</h2>
+                    <h2 class="title_color">DUSUN DESA BADES</h2>
                     <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast, </p>
                 </div>
                 <div class="row mb_30">
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room1.jpg" alt="">
+                                <img src="assets/image/room1.png" alt="">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Double Deluxe Room</h4></a>
-                            <h5>$250<small>/night</small></h5>
+                            <!-- <h5>$250<small>/night</small></h5> -->
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room2.jpg" alt="">
+                                <img src="assets/image/room2.png" alt="">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Single Deluxe Room</h4></a>
-                            <h5>$200<small>/night</small></h5>
+                            <!-- <h5>$200<small>/night</small></h5> -->
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room3.jpg" alt="">
+                                <img src="assets/image/room3.png" alt="">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Honeymoon Suit</h4></a>
-                            <h5>$750<small>/night</small></h5>
+                            <!-- <h5>$750<small>/night</small></h5> -->
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room4.jpg" alt="">
+                                <img src="assets/image/room4.png" alt="">
+                                <a href="#" class="btn theme_btn button_hover">Book Now</a>
+                            </div>
+                            <a href="#"><h4 class="sec_h4">Honeymoon Suit</h4></a>
+                            <!-- <h5>$750<small>/night</small></h5> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="accomodation_item text-center">
+                            <div class="hotel_img">
+                                <img src="assets/image/room5.png" alt="">
+                                <a href="#" class="btn theme_btn button_hover">Book Now</a>
+                            </div>
+                            <a href="#"><h4 class="sec_h4">Honeymoon Suit</h4></a>
+                            <!-- <h5>$750<small>/night</small></h5> -->
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="accomodation_item text-center">
+                            <div class="hotel_img">
+                                <img src="assets/image/room6.png" alt="">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Economy Double</h4></a>
-                            <h5>$200<small>/night</small></h5>
+                            <!-- <h5>$200<small>/night</small></h5> -->
                         </div>
                     </div>
                 </div>
